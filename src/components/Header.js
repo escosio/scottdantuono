309be11311e5799b33b3853a4aa53939d0acf3ed
Scott D'Antuono <scottd26@gmail.com>
2023-11-11 09:29:34 -0500

    update bio

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export const Header = () => {
     <div className="header section"> 
       <h1>Hello, I'm Scott D.</h1>
       <p className="subtext fade-in-text">
-        I've been doing QA for 10 years and now I want to build stuff too.
+        I've been doing QA for 10 years and now I want to build apps and tools to solve problems and make everyone's life easier.
       </p>
       <div className="socialLinks">
         <li>
@@ -29,7 +29,7 @@ export const Header = () => {
           </a>
         </li>
       </div>
-      <p >Previously, I've worked at Peloton, SeatGeek and Meetup. Over the past few months, I have completed courses in <b>AWS</b>, <b>Cypress.io</b> Web Automation and a Full-Stack course using <b>React.js, Python, Flask</b> and <b>MongoDB</b>.  My last name D'Antuono is pronounced <i>(DAN-TWAN-NOH)</i> and I live in Brooklyn, NY.</p>
+      <p className="bio-paragraph">Previously, I've worked at Peloton, SeatGeek and Meetup. I wanted to take my skills to the next level so this year I took courses in <b>AWS</b>, <b>Cypress.io</b> Web Automation, a Full-Stack course (using <b>React.js, Python, Flask</b> and <b>MongoDB</b>) and <a href="https://www.coursera.org/learn/introduction-to-android-mobile-application-development/home/welcome"><b>Android Development</b></a>!  My last name D'Antuono is pronounced <i>(DAN-TWAN-NOH)</i> and I live in Brooklyn, NY.</p>
     </div>
   );
 };
